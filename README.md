2b5f56e23ebab87c41436609145f368d1b7d7ea8
PauliusNavickas <paulius.zee@gmail.com>
2024-05-20 08:47:36 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# P53-JS30_Ll
+P-53, Js-30 paskaita, 1 lab
---
+++ styles/js/main.js
@@ -0,0 +1,35 @@
+
+// const url = 'http://167.99.138.67:1111/createaccount';
+const url = 'http://167.99.138.67:1111';
+
+// const options = {
+//     method: "POST",
+//     header: {
+//         'content-type':'application/json',
+//     },
+//     body: JSON.stringify(userData)
+// }
+//
+// function createAccounts() {
+//
+//     fetch(url, options)
+//         .then(res => res.json())
+//         .then(response => {
+//             console.log(response);
+//         })
+// }
+
+// /createaccount
+// body keys: name, passwordOne, passwordTwo
+
+// /login
+// body keys: name, password
+
+// /createpost
+// body keys: secretKey, title, image, description
+
+// /updatepost
+// body keys: secretKey, title, image, description, id
+
+// /deletepost
+// body keys: secretKey, id
